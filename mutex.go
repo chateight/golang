3c2cebdf8299de36892dd36097494a85f03ca338
rusami <isehara.jp@coderdojo.com>
2023-01-10 19:24:09 +0900

    # to use channel instead of the WaitGroup

--- mutex.go
+++ mutex.go
@@ -6,16 +6,21 @@ import (
 	"fmt"
 )
 
+//
+// to use channel instead of "WaitGroup"
+//
+// in this case, slower than WaitGroup. Channel may be useful when it takes long processing time and less inforamtion size
+//
 func main() {
-	var wg sync.WaitGroup
+	maxNumber := 10000000
 	var mu sync.Mutex
+	ch := make(chan int)
+	defer close(ch)
 	c := 2
-	odd := []int{}
+	oddCh := []int{}
 	tStart := time.Now()
-	for i := 2; i <= 10000*1000; i++ {
-		wg.Add(1)
+	for i := 2; i <= maxNumber; i++ {
 		go func() {
-			defer wg.Done()
 			flag := true		// if odd number, stay "true"
 			mu.Lock()
 			defer mu.Unlock()
@@ -26,14 +31,26 @@ func main() {
 				}
 			}
 			if flag == true{
-				odd = append(odd, c)
+				ch <- c
 			}			
 			c++
 		}()
 	}
-	wg.Wait()
+	for i:= 0; i < maxNumber; i++{
+		setBreak := false
+		select {
+		case p := <- ch:
+			oddCh = append(oddCh, p)
+		case <- time.After(time.Second):		// 1 sec wait is needed, it doesn't work if set it to 0.1sec
+			fmt.Println("Time Out!")
+			setBreak = true
+		}
+		if setBreak == true{
+			break
+		}
+	}
 	tStop := time.Now()
-	fmt.Println(len(odd))
+	fmt.Println("len : ", len(oddCh))
 
 	el := tStop.Sub(tStart)
 	fmt.Println(el)
