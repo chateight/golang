3d9f71d0595fa4fe8087149182696c5627fe1be4
rusami <isehara.jp@coderdojo.com>
2025-01-12 15:58:28 +0900

    updated

--- webServer/web_server.go
+++ webServer/web_server.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+    "fmt"
+    //"io"
     "net/http"
+    //"os"
 )
 
 func main() {
     fs := http.FileServer(http.Dir("pub"))
-    http.Handle("/", fs)
+
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        if r.URL.Path == "/" {
+            http.ServeFile(w, r, "pub/index.html")
+        } else {
+            fs.ServeHTTP(w, r)
+        }
+    })
+
+    fmt.Println("Server listening on port 4000")
     http.ListenAndServe(":4000", nil)
 }
-
